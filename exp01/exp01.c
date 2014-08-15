3fd575d75e75103410ad3aefc8cd769f339b6798
Igor Bonadio <igorbonadio@gmail.com>
2014-08-15 16:04:45 -0300

    fix bug: base class can not be a pointer

--- exp01/exp01.c
+++ exp01/exp01.c
@@ -6,10 +6,14 @@ typedef struct {
   char* name;
 } Animal;
 
-Animal* animalNew(char* name) {
-  Animal* animal = malloc(sizeof(Animal));
+void animalInit(Animal* animal, char* name) {
   animal->name = malloc(strlen(name)*sizeof(char));
   strcpy(animal->name, name);
+}
+
+Animal* animalNew(char* name) {
+  Animal* animal = malloc(sizeof(Animal));
+  animalInit(animal, name);
   return animal;
 }
 
@@ -19,47 +23,57 @@ void animalPrint(Animal* animal) {
 }
 
 typedef struct {
-  Animal* base;
+  Animal base;
   int tail;
 } Dog;
 
+void dogInit(Dog* dog, char* name, int tail) {
+  animalInit(&dog->base, name);
+  dog->tail = tail;
+}
+
 Dog* dogNew(char* name, int tail) {
   Dog *dog = malloc(sizeof(Dog));
-  dog->base = animalNew(name);
-  dog->tail = 1;
+  dogInit(dog, name, tail);
   return dog;
 }
 
 void dogPrint(Dog* dog) {
   printf("Dog:\n");
-  printf("  name: %s\n", dog->base->name);
+  printf("  name: %s\n", dog->base.name);
   printf("  tail: %d\n", dog->tail);
 }
 
 typedef struct {
-  Animal* base;
+  Animal base;
   int swim;
 } Fish;
 
+void fishInit(Fish* fish, char* name, int swim) {
+  animalInit(&fish->base, name);
+  fish->swim = 3;
+}
+
 Fish* fishNew(char* name, int swim) {
   Fish *fish = malloc(sizeof(Fish));
-  fish->base = animalNew(name);
-  fish->swim = 3;
+  fishInit(fish, name, swim);
   return fish;
 }
 
 void fishPrint(Fish* fish) {
   printf("Fish:\n");
-  printf("  name: %s\n", fish->base->name);
+  printf("  name: %s\n", fish->base.name);
   printf("  swim: %d\n", fish->swim);
 }
 
 int main () {
   Dog* poodle = dogNew("Spike", 1);
   dogPrint(poodle);
+  animalPrint((Animal*)poodle);
 
   Fish* guppy = fishNew("Linguado", 3);
   fishPrint(guppy);
+  animalPrint((Animal*)guppy);
 
   free(poodle);
   free(guppy);
